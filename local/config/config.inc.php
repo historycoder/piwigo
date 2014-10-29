7104377134e6f40060b2fe467c0cb98008a0306a
Phil Sager <psager@ohiohistory.org>
2014-10-29 09:15:27 -0400

    changed language files and metadata mapping config

--- local/config/config.inc.php
+++ local/config/config.inc.php
@@ -3,8 +3,8 @@
 /* The file does not exist until some information is entered 
 below. Once information is entered and saved, the file will be created. */
 
-$conf['show_exif']='false';
-$conf['use_exif']='false';
+$conf['show_exif']='true';
+$conf['use_exif']='true';
 
 
 ?>
